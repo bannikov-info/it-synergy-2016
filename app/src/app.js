1798813ea76d84356cbe687b1827f7559a0b2cc5
Bannikov Evgeny <bannikov.evgeny@gmail.com>
2016-02-17 19:32:32 +0500

    synergy 2016 Init

---
+++ app/src/app.js
@@ -0,0 +1,33 @@
+;(function (angular) {
+    angular
+        .module('starterApp', ['ngMaterial', 'users', 'ngRoute'])
+        .config(function($mdThemingProvider, $mdIconProvider, $routeProvider){
+
+            $mdIconProvider
+                .defaultIconSet("./assets/svg/avatars.svg", 128)
+                .icon("menu"       , "./assets/svg/menu.svg"        , 24)
+                .icon("share"      , "./assets/svg/share.svg"       , 24)
+                .icon("google_plus", "./assets/svg/google_plus.svg" , 512)
+                .icon("hangouts"   , "./assets/svg/hangouts.svg"    , 512)
+                .icon("twitter"    , "./assets/svg/twitter.svg"     , 512)
+                .icon("phone"      , "./assets/svg/phone.svg"       , 512);
+
+                $mdThemingProvider.theme('default')
+                    .primaryPalette('brown')
+                    .accentPalette('red');
+
+                $routeProvider
+                    .when('/', {
+                        templateUrl: './assets/parts/index.html'
+                    })
+                    .when('/users', {
+                        controller: 'UserController',
+                        controllerAs: 'ul',
+                        templateUrl: './assets/parts/users/index.html'
+                    })
+                    .otherwise({
+                        redirectTo: '/'
+                    })
+
+        });
+}(angular));
